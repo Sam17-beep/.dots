a4e2d1976c467e89a2e46b70a0f6acade79d1661
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2023-12-19 10:02:50 -0500

    theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,24 @@ return {
     priority = 1000,
     config = function()
       vim.g.nightflyTransparent = true
-      vim.cmd.colorscheme 'nightfly'
+      -- vim.cmd.colorscheme 'nightfly'
+    end,
+  },
+  {
+    'neanias/everforest-nvim',
+    versoin = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('everforest').setup {
+        transparent_background_level = 1,
+        ui_contrast = 'high',
+        colours_override = function(palette)
+          palette.red = '#b86466'
+        end,
+      }
+
+      vim.cmd.colorscheme 'everforest'
     end,
   },
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -72,13 +72,7 @@ return {
     -- See `:help telescope.builtin`
     vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').find_files, { desc = '[ ] search files' })
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
 
     vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
