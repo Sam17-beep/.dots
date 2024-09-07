0271f9cf4b223f733ec3f22f9c0454d390388d0f
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2024-09-07 16:51:57 -0400

    super duper nvim

---
+++ nvim/lua/plugins/java.lua
@@ -0,0 +1,47 @@
+return {
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "JavaSE-18",
+                      path = "/Users/sam/.asdf/installs/java/openjdk-18",
+                    },
+                    {
+                      name = "JavaSE-21",
+                      path = "/Users/sam/.asdf/installs/java/openjdk-21",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              root_markers = {
+                "settings.gradle",
+                "settings.gradle.kts",
+                "pom.xml",
+                "build.gradle",
+                "mvnw",
+                "gradlew",
+                "build.gradle",
+                "build.gradle.kts",
+              },
+            })
+          end,
+        },
+      },
+    },
+  },
+}
